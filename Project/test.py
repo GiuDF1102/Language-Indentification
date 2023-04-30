ae22c6a95e9f58419586d59fb66c96a383727c1c
GIULIA DI FEDE s307746 <s307746@studenti.polito.it>
2023-04-30 21:30:34 +0200

    added means and variance calculations

--- Project/test.py
+++ Project/test.py
@@ -1,6 +1,7 @@
 import data_utils as du
 import data_visualization as dv
 import dimensionality_reduction as dr
+import math_utils as mu
 
 if __name__=="__main__":
     labels, features = du.load("..\PROJECTS\Language_detection\Train.txt")
@@ -10,21 +11,24 @@ if __name__=="__main__":
         "Italian": 1
     }
     features_dict = {
-        "elem 0": 0,
-        "elem 1": 1,
-        "elem 2": 2,
-        "elem 3": 3,
-        "elem 4": 4,
-        "elem 5": 5        
+        "feature 0": 0,
+        "feature 1": 1,
+        "feature 2": 2,
+        "feature 3": 3,
+        "feature 4": 4,
+        "feature 5": 5        
     }
     features_dict_PCA = {
-        "PCA elem 0": 0,
-        "PCA elem 1": 1   
+        "PC-0": 0,
+        "PC-1": 1   
     }
 
-    #get_hist(features,labels,labels_dict, features_dict)
-    #get_scatter(features,labels,labels_dict, features_dict)
-    DP = dr.PCA(features,3)
+    no_reduction_means = mu.calcmean_classes(features, labels)
+    no_reduction_variance = mu.calcmean_variance(features, labels)
+    dv.get_hist(features,labels,labels_dict, features_dict)
+    dv.get_scatter(features,labels,labels_dict, features_dict)
+
+    DP = dr.PCA(features,2)
     #DP = LDA(features,labels,3)
     dv.get_scatter_3d(DP,2,labels)
     dv.get_scatter(DP,labels,labels_dict, features_dict_PCA)
