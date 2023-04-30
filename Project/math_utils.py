b7cf4fdf2458022c878804e09266bbaa1e869191
GIULIA DI FEDE s307746 <s307746@studenti.polito.it>
2023-04-30 21:28:50 +0200

    added variance and means functions

--- Project/math_utils.py
+++ Project/math_utils.py
@@ -1,5 +1,41 @@
+import numpy as np
+
 def FromRowToColumn(v):
     return v.reshape((v.size, 1))
 
 def calcmean(D):
     return D.mean(1) #ritorno media sulle colonne
+
+def calcmean_classes(Data, labels):
+    classes_list = []
+    classes_means = []
+    num_classes = len(np.unique(labels))
+    num_feats = len(Data)
+
+    for i in range(num_classes):
+        classes_list.append(np.array(Data[:, (labels == i)]))
+    
+    for i in range(num_classes):
+        means = []
+        for j in range(num_feats):
+            means.append(classes_list[i][j].mean())
+        classes_means.append(means)
+
+    return classes_means
+
+def calcmean_variance(Data, labels):
+    classes_list = []
+    classes_variance = []
+    num_classes = len(np.unique(labels))
+    num_feats = len(Data)
+
+    for i in range(num_classes):
+        classes_list.append(np.array(Data[:, (labels == i)]))
+    
+    for i in range(num_classes):
+        variance_class = []
+        for j in range(num_feats):
+            variance_class.append(classes_list[i][j].var())
+        classes_variance.append(variance_class)
+
+    return classes_variance
