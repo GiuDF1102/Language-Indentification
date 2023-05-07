b4c3fbe2bc7c8532d10ad1e3f52ef51b88cee00f
GIULIA DI FEDE s307746 <s307746@studenti.polito.it>
2023-05-07 09:24:03 +0200

    minor changes and added main

--- Lab Projects/Lab7/calc_grad.py
+++ Lab Projects/Lab7/calc_grad.py
@@ -1,11 +1,10 @@
 import numpy
 import scipy as sci
 import scipy.optimize as sciopt
-import sklearn.datasets as sk
 
 
 def f(coords): #(y,z) sotto forma di un narray (2,)
-        return (coords[0]+3)**2+numpy.sin(coords[0])
+        return (coords[0]+3)**2+numpy.sin(coords[0])+(coords[1]+1)**2
 
 def l_bfgs_with_approx_grad(coord):
       x,o,d=sciopt.fmin_l_bfgs_b(f,coord,approx_grad=True)
@@ -19,11 +18,16 @@ def calc_gradient(coord):#coord vettore riga
     for i in range(dim):
         c_eps=numpy.zeros(dim)
         c_eps[i]=eps
-        gradient.append((f(coord+c_eps)-f(coord+c_eps))/(2*eps))
+        gradient.append((f(coord+c_eps)-f(coord-c_eps))/(2*eps))
+        print((f(coord+c_eps)-f(coord-c_eps))/(2*eps))
         #print((f(coord+c_eps)-f(coord+c_eps))/(2*eps))
     return numpy.array(gradient)
 
 def l_bfgs_grad_given(coord):
     x,o,d=sciopt.fmin_l_bfgs_b(f(coord),coord)
     return x
-      
+
+if __name__=="__main__":
+    coord=numpy.array([0,0])
+    print(calc_gradient(coord))
+    print(l_bfgs_with_approx_grad(coord))
