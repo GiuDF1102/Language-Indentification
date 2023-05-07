18a0706195aea16a009e7c0f89d0944b08f5a7b2
GIULIA DI FEDE s307746 <s307746@studenti.polito.it>
2023-05-07 09:21:36 +0200

    added splitdb and split k

--- Project/data_utils.py
+++ Project/data_utils.py
@@ -16,3 +16,33 @@ def load(file_name):
                 pass                            
 
     return (np.array(label_list, dtype=int), np.array(features_list, dtype=float).T)
+
+def split_db(D, L,pTrain,pTest,seed=0):#versicolor=1, virginica=0
+    # 2/3 dei dati per il training----->100 per training, 50 per test
+    nTrain = int(D.shape[1]*pTrain/pTest)# split in percentuale
+    np.random.seed(seed)
+    idx = np.random.permutation(D.shape[1])
+    idxTrain = idx[0:nTrain]
+    idxTest = idx[nTrain:]
+    DTR = D[:, idxTrain]
+    DTE = D[:, idxTest]
+    LTR = L[idxTrain]
+    LTE = L[idxTest]
+    return (DTR, LTR), (DTE, LTE)  # DTR= data training, LTR= Label training
+    # DTE= Data test, LTE= label testing
+
+def split_k(X, y, k):
+    indices = np.random.permutation(len(X))
+    X, y = X[indices], y[indices]
+    
+    # Divide the data into k folds
+    folds = []
+    fold_size = len(X) // k
+    for i in range(k):
+        start, end = i * fold_size, (i+1) * fold_size
+        val_indices = list(range(start, end))
+        train_indices = list(range(0, start)) + list(range(end, len(X)))
+        print(train_indices, val_indices)
+        folds.append((train_indices, val_indices))
+    
+    return folds
