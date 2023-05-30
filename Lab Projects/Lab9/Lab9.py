05c64112287d47cfc460726ec475f856d250312c
GIULIA DI FEDE s307746 <s307746@studenti.polito.it>
2023-05-30 17:56:28 +0200

    modified lab9

--- Lab Projects/Lab9/Lab9.py
+++ Lab Projects/Lab9/Lab9.py
@@ -3,6 +3,20 @@ import sklearn.datasets as sk
 import scipy.optimize as opt
 from itertools import repeat
 
+def split_db_2to1(D, L, seed=0):#versicolor=1, virginica=0
+    # 2/3 dei dati per il training----->100 per training, 50 per test
+    nTrain = int(D.shape[1]*2.0/3.0)
+    np.random.seed(seed)
+    idx = np.random.permutation(D.shape[1])
+    idxTrain = idx[0:nTrain]
+    idxTest = idx[nTrain:]
+    DTR = D[:, idxTrain]
+    DTE = D[:, idxTest]
+    LTR = L[idxTrain]
+    LTE = L[idxTest]
+    return (DTR, LTR), (DTE, LTE)  # DTR= data training, LTR= Label training
+    # DTE= Data test, LTE= label testing
+
 def load_iris_without_setosa():
     D, L = sk.load_iris()['data'].T, sk.load_iris()['target']
     D = D[:, L != 0] #non prendo iris setosa
@@ -21,30 +35,26 @@ def modifyLabel(trainLabels):#from 0,1 to 1,-1
     return np.where(trainLabels==0,-1,1)
 
 def calcH(data,labels,K):
-    Zi=modifyLabel(labels)
-    Zj=modifyLabel(labels)
-    ZiZj=np.dot(Zi.T,Zj)
+    Z=modifyLabel(labels)
     D=expandMatrix(K,data)
     G=calcG(D)
-    """ H = np.zeros(G.shape) """
-    """ for i in range(D.shape[1]):
+    H = np.zeros(G.shape)
+    for i in range(D.shape[1]):
         for j in range(D.shape[1]):
-            H[i][j] = Z[i]*Z[j]*G[i][j] """
-    
+            H[i][j] = Z[i]*Z[j]*G[i][j]
 
-    return ZiZj*G
+    return H
 
-def J(alpha,data,labels,K):
-    H=calcH(data,labels,K)
+def J(alpha,H):
     grad=np.dot(H,alpha)-np.ones(H.shape[1])
     return (0.5*np.dot(alpha.T,np.dot(H,alpha))-np.dot(alpha,np.ones(H.shape[1])),grad)
 
 if __name__ == "__main__":
     data,labels=load_iris_without_setosa()
-    alpha=np.zeros(data.shape[1])#stessa dim del numero si sample
+    (DTR, LTR), (DTE, LTE)=split_db_2to1(data,labels)
+    alpha=np.zeros(DTR.shape[1])#stessa dim del numero si sample
 
-
+    H = calcH(DTR,LTR,1)
     C=0.1
-    bounds = list(repeat((0, C), data.shape[1]))
-    (x, f, data)=opt.fmin_l_bfgs_b(J,alpha,args=(data,labels,1),bounds=bounds)
-    print(x)
+    bounds = list(repeat((0, C), DTR.shape[1]))
+    (x, f, data)=opt.fmin_l_bfgs_b(J, alpha, args=(H,),bounds=bounds, factr=1.0)
