ef8a98e1419fec7f413d167c682df19afc10d1da
GIULIA DI FEDE s307746 <s307746@studenti.polito.it>
2023-05-31 15:17:38 +0200

    lab9 SVM

--- Lab Projects/Lab9/Lab9.py
+++ Lab Projects/Lab9/Lab9.py
@@ -49,6 +49,30 @@ def J(alpha,H):
     grad=np.dot(H,alpha)-np.ones(H.shape[1])
     return (0.5*np.dot(alpha.T,np.dot(H,alpha))-np.dot(alpha,np.ones(H.shape[1])),grad)
 
+""" def primalSolution(alpha,C,DTR,LTR,K):
+    return C*np.sum(np.maximum(0,1-modifyLabel(LTR)*np.dot(w.T,expandMatrix(K, DTR)))) """
+
+def primalSolution(w, D, C, LTR,K, f):
+    normTerm = (1/2)*(np.linalg.norm(w)**2)
+    m = np.zeros(modifyLabel(LTR.size))
+    for i in range(modifyLabel(LTR).size):
+        vett = [0, 1-modifyLabel(LTR)[i]*(np.dot(w, expandMatrix(K, D)[:, i]))]
+        m[i] = vett[np.argmax(vett)]
+    pl = normTerm + C*np.sum(m)
+    dg = pl-f
+    return  dg
+
+def primalObjective(w, D, C, LTR, f):
+    normTerm = (1/2)*(np.linalg.norm(w)**2)
+    m = np.zeros(LTR.size)
+    for i in range(LTR.size):
+        vett = [0, 1-modifyLabel(LTR)[i]*(np.dot(w.T, D[:, i]))]
+        m[i] = vett[np.argmax(vett)]
+    pl = normTerm + C*np.sum(m)
+    dl = -f
+    dg = pl-dl
+    return pl, dl, dg
+
 if __name__ == "__main__":
     data,labels=load_iris_without_setosa()
     (DTR, LTR), (DTE, LTE)=split_db_2to1(data,labels)
@@ -57,4 +81,18 @@ if __name__ == "__main__":
     H = calcH(DTR,LTR,1)
     C=0.1
     bounds = list(repeat((0, C), DTR.shape[1]))
-    (x, f, data)=opt.fmin_l_bfgs_b(J, alpha, args=(H,),bounds=bounds, factr=1.0)
+    (alpha, f, data)=opt.fmin_l_bfgs_b(J, alpha, args=(H,),bounds=bounds, factr=1.0)
+    w = np.sum((alpha*modifyLabel(LTR)).reshape(1, DTR.shape[1])*expandMatrix(1, DTR), axis=1)
+    
+    scores = np.dot(w.T, expandMatrix(1, DTE))
+    
+    LP = 1*(scores > 0)
+    # Replace 0 with -1 because of the transformation that we did on the labels
+    LP[LP == 0] = -1
+    numberOfCorrectPredictions = np.array(LP == modifyLabel(LTE)).sum()
+    accuracy = numberOfCorrectPredictions/modifyLabel(LTE).size*100
+    errorRate = 100-accuracy
+    print(errorRate)
+
+    D = expandMatrix(1, DTR)
+    print(primalObjective(w, D, C, LTR, f))
